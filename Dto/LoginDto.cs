859854bcdcd162233c5e2e7b243730b2aced2552
Klee Collage <klee.collage@gmail.com>
2025-02-25 12:07:50 -0600

    feat(dto): Add login and register DTO's

---
+++ Dto/LoginDto.cs
@@ -0,0 +1,7 @@
+namespace ApiRest.Dto;
+
+public class LoginDto
+{
+  public string Email { get; set; }
+  public string Password { get; set; }
+}
---
+++ Dto/RegisterDto.cs
@@ -0,0 +1,8 @@
+namespace ApiRest.Dto;
+
+public class RegisterDto
+{
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Password { get; set; }
+}
